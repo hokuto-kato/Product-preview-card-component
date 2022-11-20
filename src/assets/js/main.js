37a8f0952f15bff64916c02009c6588c907bc6bd
hokuto-kato <hokuto15@gmail.com>
2022-11-20 23:18:00 +0900

    [clean #23]Clean out unnecessary sources and files

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,7 @@
 import '../sass/style.sass'
 import { font } from './include/font.js'
-import { addWebpDetectionClass } from './include/webp.js'
 import { animation } from './include/animation.js'
 document.addEventListener('DOMContentLoaded', () => {
 	font()
-	addWebpDetectionClass()
 	animation()
 })
